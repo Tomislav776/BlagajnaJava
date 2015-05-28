966dc23ff87a7c74afd1ba07407d0239f53a1399
Tomislav776 <zdencec52@gmail.com>
2015-05-28 17:04:19 +0200

    rad s bazom

---
+++ Blagajna/src/controller/bazaBlagajna.java
@@ -0,0 +1,43 @@
+package controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+//import com.mysql.jdbc.Driver;
+
+public class bazaBlagajna {
+	
+	public void bazaIspis() {
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			
+			//Vezanje na bazu
+			String connectionUrl = "jdbc:mysql://localhost:3306/blagajna";
+			String connectionUser = "root";
+			String connectionPassword = "";
+			conn = DriverManager.getConnection(connectionUrl, connectionUser, connectionPassword);
+			
+			//Rad s bazom
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery("SELECT * FROM artikli");
+			while (rs.next()) {
+				int id = rs.getInt("id");
+				String naziv = rs.getString("naziv");
+				int kolicina = rs.getInt("kolicina");
+				System.out.println("ID: " + id + ", Naziv: " + naziv+ ", Kolicina: " + kolicina);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try { if (rs != null) rs.close(); } catch (SQLException e) { e.printStackTrace(); }
+			try { if (stmt != null) stmt.close(); } catch (SQLException e) { e.printStackTrace(); }
+			try { if (conn != null) conn.close(); } catch (SQLException e) { e.printStackTrace(); }
+		}
+	}
+}
