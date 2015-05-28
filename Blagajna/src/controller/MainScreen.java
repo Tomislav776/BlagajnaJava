3b364358d44a35f42fc23c9bcf6799348922c302
Tomislav776 <zdencec52@gmail.com>
2015-05-29 00:48:56 +0200

    Poboljšan izgled gumba

--- Blagajna/src/controller/MainScreen.java
+++ Blagajna/src/controller/MainScreen.java
@@ -1,15 +1,21 @@
 package controller;
 
+import javax.swing.plaf.synth.Region;
+
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
+import javafx.geometry.VPos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.RowConstraints;
 import javafx.stage.Stage;
 
 public class MainScreen{
@@ -40,13 +46,18 @@ public class MainScreen{
     	
          
         int k=-1;
-        for(int i = 0 ; i<4;i++) {
+        for(int i = 0 ; i<4;i++) {    
         	for (int j=0;j<4;j++){
         		k++;
+        		btns[k].setMinSize(149, 120);	//Poveca gumbove da popune okvir
+        		btns[k].setMaxSize(620, 500);
+        		    
         		grid_GumboviArtikl.add(btns[k],i ,j);
         		grid_GumboviArtikl.setHalignment(btns[k], HPos.CENTER);  //Centrira gumbove
+        		grid_GumboviArtikl.setValignment(btns[k], VPos.CENTER);
         		
-        		
+        		//grid_GumboviArtikl.setHgrow(btns[k], Priority.ALWAYS); 
+        		//grid_GumboviArtikl.setVgrow(btns[k], Priority.ALWAYS); 
         	}
         }
     	
