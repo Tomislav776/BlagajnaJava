2d96ae0bd127183bbc5301e0c02f9bf17e52d5e5
Tomislav776 <zdencec52@gmail.com>
2015-05-29 23:41:17 +0200

    Table View korak 1

--- Blagajna/src/controller/MainScreen.java
+++ Blagajna/src/controller/MainScreen.java
@@ -1,8 +1,10 @@
 package controller;
 
-import javax.swing.plaf.synth.Region;
-
+import dataClass.Artikli;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
@@ -10,7 +12,10 @@ import javafx.geometry.VPos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
@@ -22,6 +27,15 @@ public class MainScreen{
 	
 	@FXML
     public GridPane grid_GumboviArtikl;
+	
+	@FXML
+    public TableView<Artikli> tableViewRacun;
+	
+	@FXML
+    public TableColumn tableColumnCijena;
+	
+	@FXML
+    public TableColumn<Artikli, String> tableColumnNaziv;
 
     private Button[] btns = new Button[50];
 
@@ -44,6 +58,13 @@ public class MainScreen{
     public void initialize() {
     	initBtnsArray();
     	
+    	//inicijalizira tablicu stupce, naziv je ime podatka u klasi Artikli
+    	tableColumnNaziv.setCellValueFactory(new PropertyValueFactory<Artikli,String>("naziv"));
+    	
+    	tableViewRacun.setItems(getArtikli());
+    	
+    	
+    	
          
         int k=-1;
         for(int i = 0 ; i<4;i++) {    
@@ -57,13 +78,39 @@ public class MainScreen{
         		grid_GumboviArtikl.setValignment(btns[k], VPos.CENTER);
         		
         		//grid_GumboviArtikl.setHgrow(btns[k], Priority.ALWAYS); 
-        		//grid_GumboviArtikl.setVgrow(btns[k], Priority.ALWAYS); 
+        		//grid_GumboviArtikl.setVgrow(btns[k], Priority.ALWAYS);
+        		
+        		//Postavlja Listenere na gumbove
+        		btns[k].setOnAction (e -> {
+        			gumbArtikliKlik(e);
+        		});
+        		}
         	}
         }
+    
+    //Funkcija koja se izvodi kad se klikne na gumb
+	public void gumbArtikliKlik(ActionEvent sender){
+		Button btn=(Button) sender.getSource();
+		
+		
+		//System.out.println("Provba");
+	}
+	
+	
+	//Radi observable list stavlja artikle u nju za prikaz u table view
+	public ObservableList<Artikli> getArtikli(){
+		ObservableList<Artikli> artikli = FXCollections.observableArrayList();
+		artikli.add(new Artikli("Netko"));
+		artikli.add(new Artikli("Netko2"));
+		artikli.add(new Artikli("Netko3"));
+		
+		return artikli;
+	}
+	
     	
-    }
-    
-    
-    
 }
+    
+    
+    
+
 
---
+++ Blagajna/src/dataClass/Artikli.java
@@ -0,0 +1,18 @@
+package dataClass;
+
+public class Artikli {
+	private String naziv;
+
+	public String getNaziv() {
+		return naziv;
+	}
+
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+	
+	public Artikli (String naziv){
+		this.naziv=naziv;
+	}
+	
+}
