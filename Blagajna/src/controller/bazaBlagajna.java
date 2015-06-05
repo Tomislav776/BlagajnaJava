affd8d4618ac64a6debf8d2c92a716aebd2af030
Tomislav776 <zdencec52@gmail.com>
2015-06-06 00:07:14 +0200

    kkkkk

--- Blagajna/src/controller/bazaBlagajna.java
+++ Blagajna/src/controller/bazaBlagajna.java
@@ -14,7 +14,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-//import com.mysql.jdbc.Driver;
+//import com.mysql.jdbc.Driver; kèlkl
 
 /**
  * Razred <code>bazaBlagajna</code> omoguèava rad sa bazom podataka.
